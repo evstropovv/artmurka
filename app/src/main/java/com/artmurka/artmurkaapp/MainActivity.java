bf6f5c455c582bef8b69516a65b25b219cba04c1
evstropovv <vas.evstropov@gmail.com>
2017-06-13 23:12:41 +0300

    add library rxjava

--- app/src/main/java/com/artmurka/artmurkaapp/MainActivity.java
+++ app/src/main/java/com/artmurka/artmurkaapp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.artmurka.artmurkaapp;
 
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    FragmentTransaction fragmentTransaction;
+    PlusOneFragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button)findViewById(R.id.btnTest);
+        fragment = new PlusOneFragment();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragmentTransaction = getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.mainFrame, fragment);
+                fragmentTransaction.commit();
+
+            }
+        });
 
     }
 }
