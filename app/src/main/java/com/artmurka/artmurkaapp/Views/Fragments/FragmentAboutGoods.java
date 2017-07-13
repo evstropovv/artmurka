42cdc8c8b8b36a61a11f8771aed361fca967c965
evstropovv <vas.evstropov@gmail.com>
2017-07-13 23:58:20 +0300

    add loader

--- app/src/main/java/com/artmurka/artmurkaapp/Views/Fragments/FragmentAboutGoods.java
+++ app/src/main/java/com/artmurka/artmurkaapp/Views/Fragments/FragmentAboutGoods.java
@@ -3,14 +3,20 @@ package com.artmurka.artmurkaapp.Views.Fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.artmurka.artmurkaapp.R;
+import com.artmurka.artmurkaapp.Views.Fragments.Interfaces.IFragmentAboutGoods;
 
 
-public class FragmentAboutGoods extends Fragment {
+public class FragmentAboutGoods extends Fragment implements IFragmentAboutGoods, LoaderManager.LoaderCallbacks<String> {
+
+
+
 
     public FragmentAboutGoods() {
         // Required empty public constructor
@@ -21,7 +27,48 @@ public class FragmentAboutGoods extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_fragment_about_goods, container, false);
 
-        return inflater.inflate(R.layout.fragment_fragment_about_goods, container, false);
+        return view;
+    }
+
+    @Override
+    public void setPhoto(String url) {
+
+    }
+
+    @Override
+    public void setDescription(String description) {
+
+    }
+
+    @Override
+    public void setPrice(String price) {
+
+    }
+
+    @Override
+    public void setFullDescription(String fullDescription) {
+
+    }
+
+    @Override
+    public void setDataForRecyclerView() {
+
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+
     }
 }
---
+++ app/src/main/java/com/artmurka/artmurkaapp/Views/Fragments/Interfaces/IFragmentAboutGoods.java
@@ -0,0 +1,10 @@
+package com.artmurka.artmurkaapp.Views.Fragments.Interfaces;
+
+public interface IFragmentAboutGoods {
+    void setPhoto(String url);
+    void setDescription(String description);
+    void setPrice(String price);
+    void setFullDescription(String fullDescription);
+    void setDataForRecyclerView();
+
+}
