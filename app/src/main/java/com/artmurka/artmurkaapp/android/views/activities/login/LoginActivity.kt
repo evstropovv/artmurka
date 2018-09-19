2880f4ffbcdd1c7fad3e3e888c13225f48922fbb
vasya <vas.evstropov@gmail.com>
2018-09-19 23:45:06 +0300

    App can be started. Commit Recycler Adapters

---
+++ app/src/main/java/com/artmurka/artmurkaapp/android/views/activities/login/LoginActivity.kt
@@ -0,0 +1,173 @@
+package com.artmurka.artmurkaapp.android.views.activities.login
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.os.AsyncTask
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.CookieManager
+import android.webkit.CookieSyncManager
+import android.webkit.ValueCallback
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+
+import com.artmurka.artmurkaapp.BuildConfig
+import com.artmurka.artmurkaapp.data.model.databases.Preferences
+import com.artmurka.artmurkaapp.presenter.loginucoz.UcozApi
+import com.artmurka.artmurkaapp.presenter.loginucoz.User
+import com.artmurka.artmurkaapp.R
+import com.github.scribejava.core.builder.ServiceBuilder
+import com.github.scribejava.core.model.OAuth1AccessToken
+import com.github.scribejava.core.model.OAuth1RequestToken
+import com.github.scribejava.core.model.OAuthRequest
+import com.github.scribejava.core.model.Response
+import com.github.scribejava.core.model.Verb
+import com.github.scribejava.core.oauth.OAuth10aService
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+
+import java.io.IOException
+import java.util.concurrent.ExecutionException
+
+class LoginActivity : AppCompatActivity() {
+    private var webView: WebView? = null
+    private var verifier: String? = null
+    private var requestToken: OAuth1RequestToken? = null
+    private var service: OAuth10aService? = null
+    private var accessToken: OAuth1AccessToken? = null
+    private var user: User? = null
+
+    @SuppressLint("StaticFieldLeak")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        webView = findViewById<View>(R.id.webView)
+        webView!!.clearCache(true)
+        webView!!.settings.javaScriptEnabled = true
+        webView!!.settings.userAgentString = "Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0"
+        webView!!.settings.builtInZoomControls = true
+        webView!!.settings.displayZoomControls = false
+        service = ServiceBuilder(BuildConfig.LOGIN_CONSUMER_KEY)
+                .apiSecret(BuildConfig.LOGIN_CONSUMER_SECRET)
+                .debug()
+                .build(UcozApi.instance())
+
+        object : AsyncTask<Void, Void, String>() {
+            override fun doInBackground(vararg params: Void): String {
+                try {
+                    requestToken = service!!.requestToken
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } catch (e: ExecutionException) {
+                    e.printStackTrace()
+                }
+
+                return service!!.getAuthorizationUrl(requestToken)
+            }
+
+            override fun onPostExecute(result: String) {
+                loadURL(result)
+            }
+        }.execute()
+
+
+    }
+
+    fun loadURL(url: String) {
+        deleteCokies()
+
+        webView!!.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
+                val uri = Uri.parse(url)
+                if (url.contains("oauth_verifier")) {
+                    webView!!.visibility = webView!!.GONE
+                    verifier = uri.getQueryParameter("oauth_verifier")
+                    getAccessToken()
+                }
+                return false
+            }
+        }
+        webView!!.loadUrl(url)
+    }
+
+    private fun deleteCokies() {
+        val cookieManager = CookieManager.getInstance()
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            CookieSyncManager.createInstance(this)
+            cookieManager.removeAllCookie()
+        } else {
+            cookieManager.setAcceptCookie(true)
+            cookieManager.removeAllCookies { }
+        }
+    }
+
+    private fun getAccessToken() {
+        object : AsyncTask<Void, Void, OAuth1AccessToken>() {
+            override fun doInBackground(vararg params: Void): OAuth1AccessToken? {
+                try {
+                    accessToken = service!!.getAccessToken(requestToken, verifier)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } catch (e: ExecutionException) {
+                    e.printStackTrace()
+                }
+
+                //get user info
+                val request = OAuthRequest(Verb.GET, "http://uapi.ucoz.com/accounts/GetUserInfo")
+                service!!.signRequest(accessToken!!, request)
+                var response: Response? = null
+                try {
+                    response = service!!.execute(request)
+                    val builder = GsonBuilder()
+                    val gson = builder.create()
+                    user = gson.fromJson(response!!.body, User::class.java)
+
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                } catch (e: ExecutionException) {
+                    e.printStackTrace()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+                return accessToken
+            }
+
+            override fun onPostExecute(result: OAuth1AccessToken) {
+                val intent = Intent()
+                if (result.token != null) {
+                    Preferences.consumerKey = BuildConfig.LOGIN_CONSUMER_KEY
+                    Preferences.consumerSecret = BuildConfig.LOGIN_CONSUMER_SECRET
+                    Preferences.oauthToken = result.token
+                    Preferences.oauthTokenSecret = result.tokenSecret
+                    Preferences.name = user!!.nickname
+                    Preferences.email = user!!.email
+                    intent.putExtra("name", user!!.nickname)
+                    intent.putExtra("email", user!!.email)
+
+                } else {
+
+                    intent.putExtra("name", "Арт-Мурка")
+                    intent.putExtra("email", "artmurka.com")
+                }
+                setResult(Activity.RESULT_OK, intent)
+                finish()
+            }
+        }.execute()
+    }
+
+    override fun onBackPressed() {
+        if (webView!!.canGoBack()) {
+            webView!!.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
---
+++ app/src/main/java/com/artmurka/artmurkaapp/presenter/adapters/PagerAdapter.kt
@@ -0,0 +1,39 @@
+package com.artmurka.artmurkaapp.presenter.adapters
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentStatePagerAdapter
+
+import com.artmurka.artmurkaapp.android.views.fragments.FragmentAboutGoods
+import com.artmurka.artmurkaapp.android.views.fragments.FragmentDescriptionGoods
+
+
+class PagerAdapter(fm: FragmentManager, private val numberOfTabs: Int, private val currentId: String) : FragmentStatePagerAdapter(fm) {
+    private val bundle: Bundle
+
+    init {
+        bundle = Bundle()
+        bundle.putString("id", currentId)
+    }
+
+    override fun getItem(position: Int): Fragment? {
+        when (position) {
+            0 -> {
+                val fragmentAboutGoods = FragmentAboutGoods()
+                fragmentAboutGoods.arguments = bundle
+                return fragmentAboutGoods
+            }
+            1 -> {
+                val fragmentDescriptionGoods = FragmentDescriptionGoods()
+                fragmentDescriptionGoods.arguments = bundle
+                return fragmentDescriptionGoods
+            }
+            else -> return null
+        }
+    }
+
+    override fun getCount(): Int {
+        return numberOfTabs
+    }
+}
