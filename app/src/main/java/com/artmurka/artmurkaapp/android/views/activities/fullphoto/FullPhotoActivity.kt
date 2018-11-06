0701210f0939f06f293cb364bc663ac95b3a7707
vasya <vas.evstropov@gmail.com>
2018-11-06 20:49:39 +0200

    add arch navigation component

---
+++ app/src/main/java/com/artmurka/artmurkaapp/android/views/activities/fullphoto/FullPhotoActivity.kt
@@ -0,0 +1,33 @@
+package com.artmurka.artmurkaapp.android.views.activities.fullphoto
+
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.artmurka.artmurkaapp.R
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_fullphoto.*
+import kotlinx.android.synthetic.main.nav_header_main.*
+
+/**
+ * Created by Vasya on 11.02.2018.
+ */
+
+class FullPhotoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fullphoto)
+        ivClose.setOnClickListener { finish() }
+        val bundle = intent.extras
+        if (bundle != null) {
+            Picasso.with(this)
+                    .load(bundle.getString("image"))
+                    .placeholder(R.drawable.splash)
+                    .into(imageView, object : Callback {
+                        override fun onSuccess() {}
+                        override fun onError() {}
+                    })
+        }
+    }
+}
